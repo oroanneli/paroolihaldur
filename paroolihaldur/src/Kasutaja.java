f68f3c22c7ff73dd86c5f1b0e4e10ab4259720c8
marileelumberg <lumberg@ut.ee>
2025-03-24 18:22:30 +0200

    Lisasin lugemise/salvestamise
    Loeb kasutajaandmed failist, kasutab teist meetodit ja teeb ka sõnastiku. Kasutaja on ka olemas

---
+++ paroolihaldur/src/Kasutaja.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Kasutaja {
+    private String kasutajanimi;
+    private String master_password;
+    private HashMap<String, ArrayList<String[]>> sonastik;
+
+    public Kasutaja(String kasutajanimi, String master_password, HashMap<String, ArrayList<String[]>> sonastik) {
+        this.kasutajanimi = kasutajanimi;
+        this.master_password = master_password;
+        this.sonastik = sonastik;
+    }
+
+    public String getKasutajanimi() {
+        return kasutajanimi;
+    }
+
+    public void setKasutajanimi(String kasutajanimi) {
+        this.kasutajanimi = kasutajanimi;
+    }
+
+    public String getMaster_password() {
+        return master_password;
+    }
+
+    public void setMaster_password(String master_password) {
+        this.master_password = master_password;
+    }
+
+    public HashMap<String, ArrayList<String[]>> getSonastik() {
+        return sonastik;
+    }
+
+    public void setSonastik(HashMap<String, ArrayList<String[]>> sonastik) {
+        this.sonastik = sonastik;
+    }
+
+    @Override
+    public String toString() {
+        return "Kasutaja{" +
+                "kasutajanimi='" + kasutajanimi + '\'' +
+                ", master_password='" + master_password + '\'' +
+                '}';
+    }
+}
