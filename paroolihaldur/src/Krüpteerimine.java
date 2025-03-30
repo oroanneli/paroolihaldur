05cc6462e9bb2433045be2cfa4c6b16f0d8ecb55
Karel Konga <kongakarel@gmail.com>
2025-03-31 00:35:29 +0300

    uus krüpt sõne

--- paroolihaldur/src/Krüpteerimine.java
+++ paroolihaldur/src/Krüpteerimine.java
@@ -5,7 +5,7 @@ public class Krüpteerimine {
      * @return tagastab krüpteeritud koodi
      */
     public static String krüpteeri(String parool){
-        String kõik = "abcdefghijklmnopqrstuvwxyzöäüõABCDEFGHIJKLMNOPQRSTUVWXYZÖÄÜÕ0123456789^$*.[]{}()?-\"!@#%&/\\,><':;|_~`";
+        String kõik = "xYM@ghy[RTÖSF9~_^GjPk!#ufmcblA2WÄ1/)\"0zZ?(]HEns`BKöV.QO<õe6,4&r;NCvä{3|I*:Ü}8iL>%XJot7a'$ü-D5Upq\\Õwd";
         String krüpteeritud = "";
         char[] paroolCharArray = parool.toCharArray();
 
@@ -23,7 +23,7 @@ public class Krüpteerimine {
      * @return tagastab dekrüptitud parooli
      */
     public static String dekrüpteeri(String parool){
-        String kõik = "abcdefghijklmnopqrstuvwxyzöäüõABCDEFGHIJKLMNOPQRSTUVWXYZÖÄÜÕ0123456789^$*.[]{}()?-\"!@#%&/\\,><':;|_~`";
+        String kõik = "xYM@ghy[RTÖSF9~_^GjPk!#ufmcblA2WÄ1/)\"0zZ?(]HEns`BKöV.QO<õe6,4&r;NCvä{3|I*:Ü}8iL>%XJot7a'$ü-D5Upq\\Õwd";
         String dekrüpteeritud = "";
         char[] paroolCharArray = parool.toCharArray();
 
@@ -34,10 +34,10 @@ public class Krüpteerimine {
         }
         return dekrüpteeritud.toString();
     }
-
+    /*
     public static void main(String[] args) {
         System.out.println(krüpteeri("karupoegpuhh"));
         System.out.println(dekrüpteeri(krüpteeri("karupoegpuhh")));
     }
-
+    */
 }
