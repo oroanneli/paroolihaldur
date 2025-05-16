e9656f0a797b3fe31be4e0f09f2e9a31e050d873
anneli-oro <annelioro@ut.ee>
2025-05-16 12:38:35 +0300

    login leht teoorias funkab

---
+++ paroolihaldur_fx/src/main/LoginController.java
@@ -0,0 +1,59 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import main.Main;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class LoginController {
+    @FXML private TextField usernameField;
+    @FXML private PasswordField passwordField;
+
+    @FXML
+    private void handleLogin(ActionEvent event) throws IOException {
+        String kasutajanimi = usernameField.getText();
+        String parool = passwordField.getText();
+
+        ArrayList<Kasutaja> kasutajad = FailiTootlus.loeKasutajad("meistrid.txt"); // loeb sisse olemasolevad kasutajad
+
+        if (kontrolliParool(kasutajad, kasutajanimi, parool)) {
+            // Vaheta stseen SideBar.fxml peale
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/SideBar.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
+
+            Main.primaryStage.setScene(scene);
+            Main.primaryStage.setTitle("Paroolihaldur");
+        } else {
+            System.out.println("Vale kasutajanimi või parool!");
+            // Võib lisada vea kuvamise kasutajale
+        }
+    }
+
+
+    public static boolean kontrolliParool(ArrayList<Kasutaja> kasutajad, String kasutajanimi, String parool) { // returnib true, kui kasutajanimi ja parool klapivad
+        for (Kasutaja kasutaja : kasutajad) {
+            if (kasutaja.getKasutajanimi().equals(kasutajanimi) && kasutaja.getMaster_password().equals(parool)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+//    public static boolean kontrolliKasutajaOlemas(ArrayList<Kasutaja> kasutajad, String kasutajanimi) { // returnib true kui selline kasutaja leidub
+//        for (Kasutaja kasutaja : kasutajad) {
+//            if (kasutaja.getKasutajanimi().equals(kasutajanimi)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
+}
--- paroolihaldur_fx/src/main/Main.java
+++ paroolihaldur_fx/src/main/Main.java
@@ -9,10 +9,18 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+    public static Stage primaryStage; // oota ma ei tea
+
     public void start(Stage stage) throws IOException {
-        // Load the FXML file for the main layout
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/SideBar.fxml"));
+        primaryStage = stage;
+
+        // lae kõigepealt login
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/login.fxml"));
         Parent root = loader.load();
+
+        // Load the FXML file for the main layout
+        //FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/SideBar.fxml"));
+        //Parent root = loader.load();
 
         // Create a scene with the loaded root
         Scene scene = new Scene(root);
